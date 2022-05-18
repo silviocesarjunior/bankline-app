c0c2093d8ddc6ec84d13bc816d1d6ef4f5f40001
silviocesarjunior <silvio_czar_cpv@hotmail.com>
2022-05-17 22:13:37 -0300

    Final Frontend

--- src/app/components/movimentacao-list/movimentacao-list.component.ts
+++ src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CorrentistaService } from 'src/app/services/correntista.service';
 import { MovimentacaoService } from 'src/app/services/movimentacao.service';
 
 @Component({
@@ -7,22 +8,36 @@ import { MovimentacaoService } from 'src/app/services/movimentacao.service';
   styleUrls: ['./movimentacao-list.component.css']
 })
 export class MovimentacaoListComponent implements OnInit {
-  movimentacoes:any;
-  
-  constructor(private movimentacaoService: MovimentacaoService) { }
+  movimentacoes: any;
+  correntistas: any;
+  correntista: any;
+
+  constructor(private movimentacaoService: MovimentacaoService,
+    private correntistaService: CorrentistaService,) { }
 
   ngOnInit(): void {
-    this.listMovimentacoes();
+    this.exibirCorrentistas();
+  }
+  exibirCorrentistas(): void {
+    this.correntistaService.list()
+      .subscribe(
+        data => {
+          this.correntistas = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
   }
   listMovimentacoes(): void {
-    this.movimentacaoService.list()
-    .subscribe(
-      data => {
-        this.movimentacoes = data;
-      },
-      error => {
-        console.log(error);
-      });
+    this.movimentacaoService.findByIdConta(this.correntista.id)
+      .subscribe(
+        data => {
+          this.movimentacoes = data;
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
